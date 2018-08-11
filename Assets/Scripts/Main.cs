d0a6c50f3dbe8cfc00d5b0abcdd5de0c0f44c1ac
Carl Larsson <cjohanlarsson@gmail.com>
2018-08-11 14:22:02 -0700

    main class

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour 
+{
+	[SerializeField] List<GameObject> createOnAwake;
+
+	void Awake() 
+	{
+		foreach (var go in createOnAwake)
+			GameObject.Instantiate(go);
+	}
+
+}
