8b5a386b128b11ca38753345cec9080b682e3ce8
Carl Larsson <cjohanlarsson@gmail.com>
2018-08-11 14:15:43 -0700

    manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour 
+{
+	[Header("Vegetables")]
+	[SerializeField] Vegetable cucumberPrefab;
+
+	[Header("Entrances")]
+	[SerializeField] Transform gateEntrace;
+	[SerializeField] Transform lobbyEntrace;
+	[SerializeField] Transform sideDoorEntrace;
+
+	[Header("Pool Entrances")]
+	[SerializeField] Transform leftPoolEntrance;
+	[SerializeField] Transform middlePoolEntrance;
+	[SerializeField] Transform rightPoolEntrance;
+
+	[Header("Pool Seats")]
+	[SerializeField] Transform farLeftPoolSeat;
+	[SerializeField] Transform leftPoolSeat;
+	[SerializeField] Transform middlePoolSeat;
+	[SerializeField] Transform rightPoolSeat;
+	[SerializeField] Transform farRightPoolSeat;
+
+	private IEnumerator Start()
+	{
+
+		yield return new WaitForSeconds(1f);
+
+		var cucumber = Spawn(cucumberPrefab, gateEntrace);
+
+
+		yield return cucumber.WalkTo(middlePoolEntrance);
+
+		yield return new WaitForSeconds(2f);
+
+		yield return cucumber.JumpTo(middlePoolSeat);
+
+	}
+
+	Vegetable Spawn( Vegetable prefab, Transform spawnAt )
+	{
+		return GameObject.Instantiate(prefab, spawnAt.transform.position, spawnAt.transform.rotation);
+	}
+
+	private void OnDrawGizmos()
+	{
+		GizmosDrawTransform(gateEntrace,Color.red);
+		GizmosDrawTransform(lobbyEntrace, Color.red);
+		GizmosDrawTransform(sideDoorEntrace, Color.red);
+
+		GizmosDrawTransform(leftPoolEntrance, Color.yellow);
+		GizmosDrawTransform(middlePoolEntrance, Color.yellow);
+		GizmosDrawTransform(rightPoolEntrance, Color.yellow);
+
+		GizmosDrawTransform(farLeftPoolSeat,Color.green);
+		GizmosDrawTransform(leftPoolSeat, Color.green);
+		GizmosDrawTransform(middlePoolSeat, Color.green);
+		GizmosDrawTransform(rightPoolSeat, Color.green);
+		GizmosDrawTransform(farRightPoolSeat, Color.green);
+	}
+
+	void GizmosDrawTransform(Transform t, Color color)
+	{
+		if(t != null)
+		{
+			Gizmos.color = color;
+			Gizmos.DrawWireSphere(t.position, 0.5f);
+		}
+	}
+}
---
+++ Assets/Scripts/Vegetable.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetable : MonoBehaviour 
+{
+	[SerializeField] float walkSpeed = 6;
+	[SerializeField] float turnSpeed = 180f;
+	[SerializeField] float jumpTime = 1f;
+
+
+	public IEnumerator WalkTo(Transform dest)
+	{
+		yield return WalkTo(dest.position);
+
+		yield return TurnTo(dest.rotation);
+	}
+
+	public IEnumerator WalkTo(Vector3 dest)
+	{
+		while (true)
+		{
+			var currPos = this.transform.position;
+			if (Vector3.Distance(currPos, dest) < Mathf.Epsilon)
+			{
+				break;
+			}
+			else
+			{
+				this.transform.position = Vector3.MoveTowards(currPos, dest, Time.deltaTime * walkSpeed);
+				yield return null;
+			}
+		}
+	}
+
+	public IEnumerator TurnTo(Quaternion dest)
+	{
+		while (true)
+		{
+			var currRot = this.transform.rotation;
+
+			if (Quaternion.Angle(currRot, dest) < Mathf.Epsilon)
+			{
+				break;
+			}
+			else
+			{
+				this.transform.rotation = Quaternion.RotateTowards(currRot, dest, Time.deltaTime * turnSpeed);
+				yield return null;
+			}
+		}
+	}
+
+
+	public IEnumerator JumpTo(Transform dest)
+	{
+		var startPos = this.transform.position;
+		var startRot = this.transform.rotation;
+		float startTime = Time.time;
+
+		while (true)
+		{
+			float timePassed = Time.time - startTime;
+			if (timePassed >= jumpTime)
+			{
+				this.transform.position = dest.position;
+				this.transform.rotation = dest.rotation;
+				break;
+			}
+			else
+			{
+				float lerp = timePassed / jumpTime;
+				this.transform.position = Vector3.Lerp(startPos, dest.position, lerp) + new Vector3(0, Mathf.Sin(lerp * Mathf.PI), 0);
+				this.transform.rotation = Quaternion.Slerp(startRot, dest.rotation, lerp);
+				yield return null;
+			}
+		}
+	}
+}
