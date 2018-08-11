261d9b49107b09c2604e0601332fd74733fc6cea
Carl Larsson <cjohanlarsson@gmail.com>
2018-08-11 01:45:29 -0700

    Initial commit: throwing syringes in a hot tub

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour 
+{
+	[SerializeField] Rigidbody launchPrefab;
+	[SerializeField] Transform launchFrom;
+	[SerializeField] float launchForce;
+	[SerializeField] float launchMaxTime;
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+		{
+			var rb = GameObject.Instantiate(launchPrefab, launchFrom.position, launchFrom.rotation);
+			rb.AddForce(rb.transform.forward * launchForce, ForceMode.Impulse);// = launchFrom.forward * launchSpeed;
+			GameObject.Destroy(rb.gameObject, launchMaxTime);
+		}
+	}
+}
---
+++ Assets/Scripts/OrientToVelocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrientToVelocity : MonoBehaviour {
+
+	private bool _rotate = true;
+	private Rigidbody rb;
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (_rotate)
+			transform.rotation = Quaternion.LookRotation(rb.velocity);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		_rotate = false;
+	}
+}
