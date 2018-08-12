21ce05efc2b4cdcdcb11aeb1afb20977422ce9d7
Dan Colman <danrcolman@gmail.com>
2018-08-11 21:43:54 -0700

    -probuilder added -exploding cylinders testing

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddExplosionForce(5, transform.position, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
