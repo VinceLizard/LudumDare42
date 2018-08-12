9242b092bdf05aab5adbfec4922cbbb90ed24c11
Dan Colman <danrcolman@gmail.com>
2018-08-11 23:40:06 -0700

    -moved skin texture -full explosion test scene, with exploding cucumber prefab

---
+++ Assets/Scripts/timedExplosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timedExplosion : MonoBehaviour {
+
+    public GameObject explodingCucumber;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("BlowItUp", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BlowItUp()
+    {
+        Instantiate(explodingCucumber);
+        gameObject.SetActive(false);
+    }
+}
