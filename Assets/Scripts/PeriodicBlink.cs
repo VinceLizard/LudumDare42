cf61c8e5dc74689777a3321c50820e7429493e19
Wyatt Strain <wellwyatt@gmail.com>
2018-08-11 22:20:15 -0700

    Finished Cucumber Animations and facial expressions. Also added blink.

---
+++ Assets/Scripts/PeriodicBlink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeriodicBlink : MonoBehaviour {
+
+    public float offset = 1.0f;
+    public GameObject[] lids = new GameObject[2];
+    public bool isBlinking = true;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Blink");
+    }
+	
+	// Update is called once per frame
+	IEnumerator Blink () {
+        yield return new WaitForSeconds(offset);
+        while(isBlinking){
+            lids[0].transform.Rotate(Vector3.right *60);
+            lids[1].transform.Rotate(Vector3.right *60);
+            yield return new WaitForSeconds(0.1f);
+            lids[0].transform.Rotate(Vector3.right * -60);
+            lids[1].transform.Rotate(Vector3.right * -60);
+            yield return new WaitForSeconds(8.0f);
+        }
+	}
+}
